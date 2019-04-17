f229e70b9585e4b6a633794809bdb33580f65dd7
Daniel Kestler <daniel.kestler@medienreaktor.de>
2019-04-17 10:34:53 +0200

    Add ImportController and autoload information to composer.json

---
+++ Classes/Controller/ImportController.php
@@ -0,0 +1,22 @@
+<?php
+namespace Medienreaktor\Import\Controller;
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Mvc\Controller\ActionController;
+
+/**
+ * Controller for import module
+ */
+class ImportController extends ActionController
+{
+
+    /**
+     * Import module index action
+     *
+     * @return void
+     */
+    public function indexAction()
+    {
+
+    }
+}
